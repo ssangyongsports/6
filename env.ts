c24be84f38b5331c2a1d11ccb5f5b6d9111c97a2
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-02 23:07:15 +0800

    Update env.ts

--- env.ts
+++ env.ts
@@ -2,5 +2,5 @@ export const EnvVars = {
   SITE_NAME: '雙龍體育',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://ssangyongsports.us11.list-manage.com/subscribe?u=d2e34413aa6cbf7c94020e4ae&id=6b0083dcd5',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://ssangyongsports.us11.list-manage.com/subscribe/post?u=d2e34413aa6cbf7c94020e4ae&amp;id=6b0083dcd5&amp;f_id=00d796e0f0',
 };
