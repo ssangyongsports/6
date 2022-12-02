efe4899370fbb62c0a8c0819f0fed1770de94539
ssangyongsports <ssangyongsports1@gmail.com>
2022-12-02 10:49:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ views/HomePage/Hero.tsx
@@ -0,0 +1,105 @@
+import NextLink from 'next/link';
+import styled from 'styled-components';
+import Button from 'components/Button';
+import ButtonGroup from 'components/ButtonGroup';
+import Container from 'components/Container';
+import HeroIllustration from 'components/HeroIllustation';
+import OverTitle from 'components/OverTitle';
+import { useNewsletterModalContext } from 'contexts/newsletter-modal.context';
+import { media } from 'utils/media';
+
+export default function Hero() {
+  const { setIsModalOpened } = useNewsletterModalContext();
+
+  return (
+    <HeroWrapper>
+      <Contents>
+        <CustomOverTitle>ssangyong sports</CustomOverTitle>
+        <Heading>我們的使命是將雙龍體育組成最棒體育組織</Heading>
+        <CustomButtonGroup>
+          <Button onClick={() => setIsModalOpened(true)}>
+            Subscribe to the newsletter <span>&rarr;</span>
+          </Button>
+          <NextLink href="#whitepaper" passHref>
+            <Button transparent>
+              Features <span>&rarr;</span>
+            </Button>
+          </NextLink>
+        </CustomButtonGroup>
+      </Contents>
+      <ImageContainer>
+        <HeroIllustration />
+      </ImageContainer>
+    </HeroWrapper>
+  );
+}
+
+const HeroWrapper = styled(Container)`
+  display: flex;
+  padding-top: 5rem;
+
+  ${media('<=desktop')} {
+    padding-top: 1rem;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const Contents = styled.div`
+  flex: 1;
+  max-width: 60rem;
+
+  ${media('<=desktop')} {
+    max-width: 100%;
+  }
+`;
+
+const CustomButtonGroup = styled(ButtonGroup)`
+  margin-top: 4rem;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  svg {
+    max-width: 45rem;
+  }
+
+  ${media('<=desktop')} {
+    margin-top: 2rem;
+    justify-content: center;
+    svg {
+      max-width: 80%;
+    }
+  }
+`;
+
+const Description = styled.p`
+  font-size: 1.8rem;
+  opacity: 0.8;
+  line-height: 1.6;
+
+  ${media('<=desktop')} {
+    font-size: 1.5rem;
+  }
+`;
+
+const CustomOverTitle = styled(OverTitle)`
+  margin-bottom: 2rem;
+`;
+
+const Heading = styled.h1`
+  font-size: 7.2rem;
+  font-weight: bold;
+  line-height: 1.1;
+  margin-bottom: 4rem;
+  letter-spacing: -0.03em;
+
+  ${media('<=tablet')} {
+    font-size: 4.6rem;
+    margin-bottom: 2rem;
+  }
+`;
