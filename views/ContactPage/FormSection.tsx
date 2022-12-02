1c43666cb2de55787bbc6ba09e199f30d92c6ea4
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-03 00:58:42 +0800

    Update FormSection.tsx

--- views/ContactPage/FormSection.tsx
+++ views/ContactPage/FormSection.tsx
@@ -43,7 +43,7 @@ export default function FormSection() {
   const isDisabled = isSubmitting || isSent;
   const isSubmitDisabled = Object.keys(errors).length > 0 || isDisabled;
 
-  if (hasSuccessfullySentMail) {
+  if (hasSuccessfullySentMail,hasErrored) {
     return <MailSentState />;
   }
 
