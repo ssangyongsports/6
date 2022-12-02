6fd795b55939996cb5f556723675b104241f25d1
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-02 21:56:39 +0800

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo({ ...rest }) {
   return (
 <img
-  src="https://example.com/media/photo.jpg"
+  src="https://img.ssangyongsports.eu.org/logo2.png"
   width={155}
   height={40}
   alt="一張圖片"
