46fb567c8d8cdfa55fb69fdea1a712fce8d99466
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-03 19:39:25 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="https://ssangyongsportsweb.blogspot.com/favicon.ico" />
         {/* <link rel="alternate" type="application/rss+xml" href={EnvVars.URL + 'rss'} title="RSS 2.0" /> */}
         {/* <script
           dangerouslySetInnerHTML={{
