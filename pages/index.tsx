c1e7ede9ab57cb3fe3f147b4788f8ef69b286314
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-04 12:24:42 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,12 +25,12 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
       <HomepageWrapper>
         <WhiteBackgroundContainer>
           <Hero />
-          <BasicSection imageUrl="https://74-rho.vercel.app/images/62d564600ff95899fb6eda9d_75df1528-2822-4edf-9c6a-8cd50c0c2c28-p-500.png" title="雙龍職棒" overTitle="SLB">
+          <BasicSection imageUrl="/1.png" title="雙龍職棒" overTitle="SLB">
             <p>
  棒球，（英語：Baseball、日韓稱為野球），為一種團體球類運動，由人數最少為9人的两支队伍在一個扇形的球場进行攻擊與守備。棒球球員分為攻...
             </p>
           </BasicSection>
-          <BasicSection imageUrl="https://74-rho.vercel.app/images/62d56332e87b47c78b67ac6c_taxi-basketball.png" title="雙龍職籃" overTitle="SBA" reversed>
+          <BasicSection imageUrl="/2.png" title="雙龍職籃" overTitle="SBA" reversed>
             <p>
               籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止對方獲得控球權或得分。籃球是世界上最多人觀看
             </p>
