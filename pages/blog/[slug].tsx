5b8c46b983d688c1fe30f14c55f25545b6e1bea7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-26 22:26:37 +0800

    Update [slug].tsx

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -55,7 +55,7 @@ export default function SingleArticlePage(props: InferGetStaticPropsType<typeof 
   if (!data) {
     return null;
   }
-  const { title, description, date, tags, imageUrl } = data.getPostsDocument.data as NonNullableChildrenDeep<Posts>;
+  const { title, description, date, tags, imageUrl, author } = data.getPostsDocument.data as NonNullableChildrenDeep<Posts>;
   const meta = { title, description, date: date, tags, imageUrl, author: '' };
   const formattedDate = formatDate(new Date(date));
   const absoluteImageUrl = imageUrl.replace(/\/+/, '/');
@@ -126,6 +126,7 @@ export async function getStaticProps({ params }: GetStaticPropsContext<{ slug: s
           title
           description
           date
+          author
           tags
           imageUrl
           body
