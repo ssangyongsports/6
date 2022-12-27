ea482729293f02df508f8625df4eca4251306487
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-27 22:23:53 +0800

    Update authorCard.js

--- components/authorCard.js
+++ components/authorCard.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function AuthorCard({ author }) {
+export default function AuthorCard
   return (
     <div className="px-8 py-8 mt-3 text-gray-500 rounded-2xl bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
       <div className="flex flex-wrap items-start sm:space-x-6 sm:flex-nowrap">
