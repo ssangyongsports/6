321f03bb3d5af83a979ef11ad2e6cc27bbcb3ae2
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-27 21:57:56 +0800

    Update pricing.tsx

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,23 +1,16 @@
 import styled from 'styled-components';
+import Page from 'components/Page';
 import FaqSection from 'views/PricingPage/FaqSection';
 import PricingTablesSection from 'views/PricingPage/PricingTablesSection';
-import Head from 'next/head';
 
 export default function PricingPage() {
   return (
-    <>
-      <Head>
-        <title>購買TV</title>
-        <meta
-          name="description"
-          content="​雙龍職籃 ... 籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止 ..."
-        />
-      </Head>
-       <Wrapper>
+    <Page title="購買TV">
+      <Wrapper>
         <PricingTablesSection />
         <FaqSection />
       </Wrapper>
-    </>
+    </Page>
   );
 }
 
